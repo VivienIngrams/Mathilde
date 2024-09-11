6220885c2c6a5c91ed1dbcf52cf8e5a9fb3cf279
VivienIngrams <vivingrams@gmail.com>
2024-09-11 17:33:10 +0100

    ADjustments

--- app/project/mirante/page.tsx
+++ app/project/mirante/page.tsx
@@ -80,11 +80,11 @@ export default async function MirantePage() {
     </div>
 
     {/* Affranchie */}
-    <div className="md:grid md:grid-cols-3">
-      <div className="flex flex-col justify-center items-start md:items-center h-[15vh] md:min-h-[100vh] w-full md:w-full">
+    <div className="flex flex-col md:grid md:grid-cols-3">
+      <div className="flex flex-col justify-center md:items-end h-[10vh] md:min-h-[100vh] w-full md:w-full">
         <h1>{projectMirante.mirante[1].title}</h1>
       </div>
-      <div className="flex flex-col justify-center items-start h-[55vh] md:min-h-[100vh] w-full">
+      <div className="flex flex-col justify-center items-start h-[50vh] md:min-h-[100vh] w-full">
         <div className="relative h-[95%] w-full">
           <Image
             src={urlFor(projectMirante.mirante[1].image).url() as string}
@@ -101,7 +101,7 @@ export default async function MirantePage() {
     </div>
 
     {/* Luxuriance */}
-    <div className="h-[45vh] md:min-h-screen w-full relative">
+    <div className="h-[30vh]  md:min-h-screen w-full relative">
       {projectMirante.mirante[2].image && (
         <Image
           src={urlFor(projectMirante.mirante[2].image).url() as string}
@@ -111,12 +111,12 @@ export default async function MirantePage() {
         />
       )}
     </div>
-    <div className="md:grid md:grid-cols-3">
-      <div className="flex flex-col justify-center items-start md:items-center h-[15vh] md:min-h-[30vh] w-full">
+    <div className="flex flex-col  md:grid md:grid-cols-3">
+      <div className="md:flex md:flex-col md:justify-center md:items-end  md:min-h-[30vh] w-full">
         <h1>{projectMirante.mirante[2].title}</h1>
       </div>
-      <div className="flex flex-col justify-center items-start md:items-center h-[55vh] md:h-[30vh] w-full col-span-2">
-        <div className="md:columns-2 gap-8">
+      <div className="md:flex md:flex-col md:justify-center md:items-center md:h-[30vh] w-full md:col-span-2">
+        <div className="md:columns-2 md:gap-8">
           <PortableText value={projectMirante.mirante[2].content} />
         </div>
       </div>
