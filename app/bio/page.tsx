061cfb773cdd1165aa6f7e1615a570ff551f589c
VivienIngrams <vivingrams@gmail.com>
2024-09-25 11:46:45 +0100

    Bio page

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -6,63 +6,110 @@ export default async function Bio() {
   return (
     <div className="h-full">
       <NavMenu />
-      <div className="bg-[rgba(227,224,220,0.70)] md:pt-20 min-h-screen">
-        
+      <div className="bg-[rgba(227,224,220,0.70)] min-h-screen">
         {/* Layout Section */}
-        <div className="grid grid-cols-1 lg:grid-cols-10 md:mx-12">
+        <div className="grid grid-cols-1 lg:grid-cols-10 md:mr-12">
           {/* Center Column: Image */}
-          <div className="relative w-full min-h-[40vh] lg:min-h-[70vh] lg:max-h-[vh] 
-          my-4 md:col-span-3">
+          <div
+            className="relative w-full min-h-[40vh] md:max-h-[92vh] my-4 md:my-0 md:col-span-4"
+          >
             <Image
               src="/portrait.jpg"
               alt="Portrait of Mathilde"
               height={500}
               width={300}
-              className="h-full w-full object-cover px-4 md:mx-auto"
+              className="h-full w-full object-cover px-24 md:mx-auto"
               loading="lazy"
             />
           </div>
-          <div className="col-span-1"></div>
+
           {/* Left Column: First half of the text */}
-          <div className="leading-relaxed text-lg lg:pr-4 col-span-3">
-            <p>
-              <span className="font-semibold italic">J’ai rencontré Mathilde</span> peu de temps après son arrivée au Portugal, elle venait à peine de poser ses valises qu’elle ouvrait déjà grands les yeux pour capter la magie ambiante de Porto et de la nature environnante.
-            </p>
-            <p>
-              C’est en la voyant prendre des photos artistiques de notre quinta, située dans la vallée du Douro, que j’ai réalisé quel type de photographe elle était. <span className="font-semibold italic">Mathilde absorbe le monde qui l’entoure</span> et le restitue en lui donnant une touche poétique très personnelle. Elle se dilue dans le décor avant d’en livrer les éléments les plus insolites qui sont souvent les plus parlants.
-            </p>
-            <p>
-              Sa relation à la nature est très forte, elle la défend en en révélant la grandeur absolue, capturée dans les détails les plus ténus.
-            </p>
-            <p>
-              Il en va de même de son rapport à l’humain. C’est en amorçant une collaboration photo-poésie sur <span className="font-semibold italic">le mode onirique que l’étendue de l’imaginaire de Mathilde m’est apparu, sans limites.</span> Notre rêverie en commun a engendré des clichés improbables, sans rapport apparents, tels qu’un phare Saint-pierre-et-miquelonais, un homme nu dans une forêt ou une tente touarègue…
-            </p>
-            <p>
-              Et pourtant, le fil conducteur, aussi peu visible soit-il, se déroule tout naturellement sous l’œil du spectateur. C’est dans cette création et cette recréation débridées que réside le talent unique de Mathilde.
-            </p>
+          <div className="flex items-end lg:pr-4 col-span-3 space-y-1">
+            <div>
+              <p>
+                <span className="font-semibold italic">
+                  J’ai rencontré Mathilde
+                </span>{" "}
+                peu de temps après son arrivée au Portugal, elle venait à peine
+                de poser ses valises qu’elle ouvrait déjà grands les yeux pour
+                capter la magie ambiante de Porto et de la nature environnante.
+              </p>
+              <p>
+                C’est en la voyant prendre des photos artistiques de notre
+                quinta, située dans la vallée du Douro, que j’ai réalisé quel
+                type de photographe elle était.{" "}
+                <span className="font-semibold italic">
+                  Mathilde absorbe le monde qui l’entoure
+                </span>{" "}
+                et le restitue en lui donnant une touche poétique très
+                personnelle. Elle se dilue dans le décor avant d’en livrer les
+                éléments les plus insolites qui sont souvent les plus parlants.
+              </p>
+              <p>
+                Sa relation à la nature est très forte, elle la défend en en
+                révélant la grandeur absolue, capturée dans les détails les plus
+                ténus.
+              </p>
+              <p>
+                Il en va de même de son rapport à l’humain. C’est en amorçant
+                une collaboration photo-poésie sur{" "}
+                <span className="font-semibold italic">
+                  le mode onirique que l’étendue de l’imaginaire de Mathilde
+                  m’est apparu, sans limites.
+                </span>{" "}
+                Notre rêverie en commun a engendré des clichés improbables, sans
+                rapport apparents, tels qu’un phare Saint-pierre-et-miquelonais,
+                un homme nu dans une forêt ou une tente touarègue…
+              </p>
+              
+            </div>
           </div>
 
-          
-
           {/* Right Column: Second half of the text */}
-          <div className=" leading-relaxed text-lg lg:pl-4 col-span-3">
-           
+          <div className="flex items-end lg:pl-4 col-span-3 space-y-1">
+            <div>
             <p>
-              La découverte de ses travaux antérieurs et en cours m’ont conforté dans ce sentiment. Elle s’est dans un premier temps intéressée à l’insularité et aux territoires isolés, notamment à travers un documentaire sur l’archipel de Saint-pierre-et-Miquelon (« Les onze mille vierges »).
-            </p>
-            <p>
-              Elle a ensuite réalisé un livre par correspondance en se prêtant au jeu du cadavre exquis avec une écrivaine restée à Paris. Cet ouvrage est un dialogue fugace photo/écriture qui questionne la notion de solitude.
-            </p>
-            <p>
-              Ses travaux incluent également « En Suspension (S) », qui s’inscrit dans une démarche duale tout aussi libérée, et « Sentiment océanique » qui nous rappelle l’urgence climatique.
-            </p>
-            <p>
-              Pour parler de la forme, <span className="font-semibold italic">la pratique artistique de Mathilde s’articule autour de la photographie argentique.</span> Ses travaux oscillent entre photographie d’ambiance et photographie plasticienne, et ils sont souvent exposés dans des lieux inattendus, à son image.
-            </p>
-            <p>
-              J’invite les amoureux de la poésie, de l’intimisme et de la singularité à y croiser le regard de Mathilde.
-            </p>
-            <p className="font-bold font-serif text-xl">Michel Le Roux, poète et ami</p>
+                Et pourtant, le fil conducteur, aussi peu visible soit-il, se
+                déroule tout naturellement sous l’œil du spectateur. C’est dans
+                cette création et cette recréation débridées que réside le
+                talent unique de Mathilde.
+              </p>
+              <p>
+                La découverte de ses travaux antérieurs et en cours m’ont
+                conforté dans ce sentiment. Elle s’est dans un premier temps
+                intéressée à l’insularité et aux territoires isolés, notamment à
+                travers un documentaire sur l’archipel de
+                Saint-pierre-et-Miquelon (« Les onze mille vierges »).
+              </p>
+              <p>
+                Elle a ensuite réalisé un livre par correspondance en se prêtant
+                au jeu du cadavre exquis avec une écrivaine restée à Paris. Cet
+                ouvrage est un dialogue fugace photo/écriture qui questionne la
+                notion de solitude.
+              </p>
+              <p>
+                Ses travaux incluent également « En Suspension (S) », qui
+                s’inscrit dans une démarche duale tout aussi libérée, et «
+                Sentiment océanique » qui nous rappelle l’urgence climatique.
+              </p>
+              <p>
+                Pour parler de la forme,{" "}
+                <span className="font-semibold italic">
+                  la pratique artistique de Mathilde s’articule autour de la
+                  photographie argentique.
+                </span>{" "}
+                Ses travaux oscillent entre photographie d’ambiance et
+                photographie plasticienne, et ils sont souvent exposés dans des
+                lieux inattendus, à son image.
+              </p>
+              <p>
+                J’invite les amoureux de la poésie, de l’intimisme et de la
+                singularité à y croiser le regard de Mathilde.
+              </p>
+              <p className="font-bold font-serif text-xl">
+                Michel Le Roux, poète et ami
+              </p>
+            </div>
           </div>
         </div>
       </div>
